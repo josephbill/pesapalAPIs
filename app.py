959781ecbe412a8f31b31beefe4dce4220adb793
josephbill <josephbill00@gmail.com>
2024-11-18 11:22:54 +0300

    added credentials for ipn registration

--- app.py
+++ app.py
@@ -34,8 +34,34 @@ def get_pesapaltoken():
     except requests.exceptions.RequestException as e:
         return jsonify({"error" : str(e)}), 500
     
+@app.route('/registerIPNURL', methods=['POST'])
+def register_ipn():
+    ipn_url = f"{base_url}/api/URLSetup/RegisterIPN"
     
+    data = request.get_json()
+    session_token = data.get("sessionToken")
+    # redirect URL : callback URL that pesapal sends the notifications to
+    redirect_url = "https://friendly-nougat-e4e413.netlify.app/" # simple hosted .html page that pesapal can link to 
     
+    # prep IPN request payload 
+    ipn_request_payload = {
+        "url" : redirect_url,
+        "ipn_notification_type": "GET"
+    }
+    
+    try:
+        headers = {
+            "Content-Type": "application/json",
+            "Accept" : "application/json",
+            "Authorization" : f"Bearer {session_token}"
+        }
+        response = requests.post(ipn_url,json=ipn_request_payload,headers=headers)
+        
+        response.raise_for_status()
+        return jsonify(response.json()), response.status_code
+        
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error" : str(e)}), 500
 
 if __name__ == '__main__':
     app.run(port=5000)
